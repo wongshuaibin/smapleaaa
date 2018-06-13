782746b523117b92ad7ff865ba95ce1797eb246e
wongshuaibin <wongshuaibin@gmail.com>
2018-06-13 09:19:24 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Sampleaaa App') - 自己动手第一遍</title>
+  <title>@yield('title', 'Sampleaaa App') - 自己动手第一遍</title>
+  <link rel="stylesheet"  href="/css/app.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-   @yield('content')
+  @include('layouts._header')
+
+  <div class="container">
+    <div class="col-md-offset-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
+  </div>
 </body>
 </html>
