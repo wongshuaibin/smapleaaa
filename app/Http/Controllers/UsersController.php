44db3dddc0c78909277e92b4b7fdb6e70274439c
wongshuaibin <wongshuaibin@gmail.com>
2018-06-14 13:44:28 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
-    //创建用户
+    //创建用户页
     public function create()
     {
         return view('users.create');
@@ -35,6 +36,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 //注册页
 Route::get('/signup', 'UsersController@create')->name('signup');
-
 //用户控制器详见入门教程6.2章
 Route::resource('users', 'UsersController');
+
+//显示登录页面
+Route::get('login', 'SessionsController@create')->name('login');
+//创建新会话(登录)
+Route::post('login', 'SessionsController@store')->name('login');
+//销毁会话(退出登录)
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
