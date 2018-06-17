f6d2a410b60530a0377ff9f5f5b162f81c8d0b62
wongshuaibin <wongshuaibin@gmail.com>
2018-06-17 15:41:59 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,12 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //微博操作
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+//关注列表和粉丝列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//关注和取关
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
+
